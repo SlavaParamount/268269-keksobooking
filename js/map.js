71368dddca28b2c4a6af2699745a4e77a43e9825
Vyacheslav <viparamount@gmail.com>
2017-09-04 09:15:09 +0300

    добавил функционал перемещения пина

--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@
   var tokyoPinMap = document.querySelector('.tokyo__pin-map');
   var pinFragment = document.createDocumentFragment();
   var RANDOM_ID;
+  var mainPin = tokyoPinMap.querySelector('.pin__main');
 
   for (var i = 0; i < window.utils.NUMBER_OF_ADS; i++) {
     pinFragment.appendChild(window.pin.generatePin(i));
@@ -37,6 +38,42 @@
     }
   };
 
+  var onMainPinHandle = function (evt) {
+    evt.preventDefault();
+
+    var startPosition = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startPosition.x - moveEvt.clientX,
+        y: startPosition.y - moveEvt.clientY
+      };
+
+      startPosition = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+  mainPin.addEventListener('mousedown', onMainPinHandle);
   tokyoPinMap.addEventListener('keydown', onOpenDialogEnterPress);
   tokyoPinMap.addEventListener('click', onOpenDialogClick);
 }());
