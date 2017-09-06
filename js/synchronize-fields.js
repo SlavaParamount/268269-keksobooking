7705118b7bd95c3a5d760ca5732abd79240c28e4
Vyacheslav <viparamount@gmail.com>
2017-09-06 22:28:00 +0300

    оптимизировал функцию сихронизации полей

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,13 +1,7 @@
 'use strict';
-(function () {
-  window.synchronizeFields = function (fieldOne, fieldTwo, valuesOne, valuesTwo, synchValues) {
-    var valueOne = fieldOne.value;
-    var valueTwo;
-    valuesOne.forEach(function (item, i) {
-      if (item === valueOne) {
-        valueTwo = valuesTwo[i];
-      }
-    });
-    synchValues(fieldTwo, valueTwo);
-  };
-}());
+window.synchronizeFields = function (fieldOne, fieldTwo, valuesOne, valuesTwo, synchValues) {
+  var valueOne = fieldOne.value;
+  var valueTwo;
+  valueTwo = valuesTwo[valuesOne.indexOf(valueOne)];
+  synchValues(fieldTwo, valueTwo);
+};
